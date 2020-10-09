8ceafb41341e5878a46b70235545a6f351d89c3e
Nick Harbaugh <nickharb@nicks-mbp.lan>
2020-10-09 09:27:25 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# UM Mapbox Demo
+A demo of the Mapbox mapping library for the Intro to Mapbox workshop at the University of Miami
---
+++ js/main.js
@@ -0,0 +1,16 @@
+/**
+ * UM Mapbox Demo
+ *
+ * Created by Seven Mile Media
+ * http://sevenmilemedia.com/
+ */
+
+(function($) {
+
+    $(document).ready(function() {
+
+        // Silence is golden.
+
+    });
+
+})(jQuery);
