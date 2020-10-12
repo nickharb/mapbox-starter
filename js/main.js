60f65971e998ae04ae97237befd2a51cc9858f8f
Nick Harbaugh <nickharb@nicks-mbp.lan>
2020-10-12 11:42:56 -0500

    Add map quickstart code

--- js/main.js
+++ js/main.js
@@ -9,6 +9,14 @@
 
     $(document).ready(function() {
 
+        mapboxgl.accessToken = 'pk.eyJ1Ijoibmlja2hhcmIiLCJhIjoiY2pucnN4cWloMGJveTNxbjJ4dzg3dGM4eCJ9.YHcYBuehFvoDyGiJr6dBig';
+        var map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+            center: [-73.944, 40.746], // starting position [lng, lat]
+            zoom: 12 // starting zoom
+        });
+
     });
 
 })(jQuery);
